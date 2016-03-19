25a833c9e227e1f2b1c0f37073513499a5a1eec3
Jonas Sjöberg <jomeganas@gmail.com>
2016-03-19 18:29:55 +0100

    Done with sudo section. Start ntp configuration.

---
+++ report/tex/ntp-aptitude-search.sh
@@ -0,0 +1,6 @@
+jonas@debian:~$ aptitude search '(^ntp)'
+i   ntp         - Network Time Protocol daemon and utility programs                                     
+p   ntp-doc     - Network Time Protocol documentation                                                   
+i   ntpdate     - client for setting system time from NTP servers                                       
+p   ntpstat     - show network time protocol (ntp) status                                               
+jonas@debian:~$
