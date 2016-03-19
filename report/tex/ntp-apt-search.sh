8f2fa07299dd098ccc99a1288aac62945ed2658d
Jonas Sjöberg <jomeganas@gmail.com>
2016-03-19 17:43:25 +0100

    Update ..

---
+++ report/tex/ntp-apt-search.sh
@@ -0,0 +1,5 @@
+jonas@debian:~$ apt-cache search ntp | grep "^ntp"
+ntp - Network Time Protocol daemon and utility programs
+ntp-doc - Network Time Protocol documentation
+ntpdate - client for setting system time from NTP servers
+ntpstat - show network time protocol (ntp) status
