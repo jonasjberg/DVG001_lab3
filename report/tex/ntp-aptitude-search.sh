445db4d4474ae2d366febd7750214fef0939bee6
Jonas Sjöberg <jomeganas@gmail.com>
2016-03-20 19:53:02 +0100

    Fix minted parser error markings.
    Fixes red boxes surrounding '$' due to minted (pygments) parser errors
    by adding trailing whitespace.

--- report/tex/ntp-aptitude-search.sh
+++ report/tex/ntp-aptitude-search.sh
@@ -3,4 +3,4 @@ i   ntp         - Network Time Protocol daemon and utility programs
 p   ntp-doc     - Network Time Protocol documentation                                                   
 i   ntpdate     - client for setting system time from NTP servers                                       
 p   ntpstat     - show network time protocol (ntp) status                                               
-jonas@debian:~$
+jonas@debian:~$ 
--- report/tex/part3-verify.sh
+++ report/tex/part3-verify.sh
@@ -4,4 +4,4 @@ drwx------  2  root    gibson  4.0K  Mar  19  21:12  /tmp/del3/a1
 -rwxr--r--  1  gibson  root    0     Mar  19  21:22  /tmp/del3/a2
 drwxr--r--  2  gibson  gibson  4.0K  Mar  19  21:27  /tmp/del3/a3
 -rwxrwx---  1  root    root    0     Mar  19  21:28  /tmp/del3/a4
-gibson@debian:/tmp$
+gibson@debian:/tmp$ 
